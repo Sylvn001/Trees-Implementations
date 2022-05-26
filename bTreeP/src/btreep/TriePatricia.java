cb206ed0ed2791f9624777828d1f694c52683753
Junior Barros <juniorbaarros@gmail.com>
2022-05-26 02:30:34 -0300

    wip: test another way to implement insertion

--- bTreeP/src/btreep/TriePatricia.java
+++ bTreeP/src/btreep/TriePatricia.java
@@ -9,7 +9,7 @@ public class TriePatricia {
     private int M = 26;
 
     public TriePatricia() {
-        raiz = null;
+        raiz = new No();
     }
 
     public No getRaiz() {
@@ -29,42 +29,60 @@ public class TriePatricia {
     }
 
     public void inserir(String palavra){
-        if(raiz == null){
-            raiz = new No();
-        }
-        else
-        {
-            int indexPalavra = 0;
-            boolean flagFim = false;
-            No noPalavra, aux = raiz;
+        int indexPalavra = 0, i, j;
+        boolean flagFim = false;
+        No noPalavra, aux = raiz;
 
-            while(!flagFim){
-                int pos = palavra.toLowerCase().charAt(indexPalavra) - 'a';
+        while(!flagFim){
+            int pos = palavra.toLowerCase().charAt(indexPalavra) - 'a';
+            if(aux.getvLig(pos) == null)
+            {
+                String restoPalavra = leString(palavra, indexPalavra);
 
-                if(aux.getvLig(pos) == null)
-                {
-                    String restoPalavra="";
-                    for(int i=indexPalavra; i < palavra.length(); i++){
-                        restoPalavra += palavra.charAt(i);
-                    }
-
-                    flagFim = true;
-                    noPalavra = new No(restoPalavra);
-                    noPalavra.setFlag(flagFim);
-                    aux.setvLig(noPalavra, pos);
-                    aux.setTL(aux.getTL()+1);
-                }
-                else
-                {
-                    aux = aux.getvLig(pos);
-                    indexPalavra++;
-                }
+                flagFim = true;
+                noPalavra = new No(restoPalavra);
+                aux.setvLig(noPalavra, pos);
+//                System.out.println("Adicionado: " + restoPalavra + " PosgetVLig: " + pos );;
+                aux.setTL(aux.getTL()+1);
+            }
+            else
+            {
+                No paiDiv =  new No (""+palavra.toLowerCase().charAt(indexPalavra));
+                No auxFilho = aux.getvLig(pos);
+                String palavraAux = leString(aux.getvLig(pos).getPalavra(), 1); 
+                System.out.println(palavraAux + " - " + paiDiv.getPalavra());
+                aux.setvLig(paiDiv, pos);
+                aux.getvLig(pos).setvLig(auxFilho, pos);
+//                String letraAnterior="";;
+//                i = 0;
+//                while(aux.getvLig(pos).getPalavra().toLowerCase().charAt(i) == palavra.toLowerCase().charAt(indexPalavra)){
+//                    letraAnterior = "" + aux.getvLig(pos).getPalavra().toLowerCase().charAt(i);
+//                    System.out.println(aux.getvLig(pos).getPalavra().charAt(i) + " igual " + palavra.charAt(indexPalavra));;;
+//                    indexPalavra++;
+//                    i++;
+//                }
+//                
+//                if(letraAnterior != aux.getvLig(pos).getPalavra()){
+//                    System.out.println(aux.getvLig(pos).getPalavra().charAt(i) + " - " + palavra.charAt(indexPalavra));
+//                    System.out.println(i);
+//                }
+//                aux = aux.getvLig(pos);
             }
         }
+        
     }
 
     public void exibir(){
         System.out.println("raiz");
+    }
+    
+    public String leString(String palavra, int pos){
+        String palavraAux = "";
+        for(int i = pos; i < palavra.length(); i++ ){
+            palavraAux += palavra.charAt(i);
+        }
+        
+        return palavraAux;
     }
 
     //.....................
